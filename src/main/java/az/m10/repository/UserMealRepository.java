97c83998fac67bfe15b322f3d978cef8893ca232
abbaszade <muradabbaszade6@gmail.com>
2025-04-16 22:54:54 +0400

    update user meal repository

--- src/main/java/az/m10/repository/UserMealRepository.java
+++ src/main/java/az/m10/repository/UserMealRepository.java
@@ -5,6 +5,8 @@ import az.m10.domain.enums.MealType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 
@@ -13,7 +15,16 @@ public interface UserMealRepository extends JpaRepository<UserMeal, Long> {
 
     Page<UserMeal> findByUserIdAndDate(Long userId, LocalDate date, Pageable pageable);
 
-    Page<UserMeal> findByUserIdAndDateAndType(Long userId, LocalDate date, MealType type, Pageable pageable);
+    @Query("SELECT um FROM UserMeal um " +
+            "JOIN Meal m ON um.mealId = m.id " +
+            "WHERE um.userId = :userId " +
+            "AND um.date = :date " +
+            "AND m.type = :type")
+    Page<UserMeal> findByUserIdAndDateAndMealType(@Param("userId") Long userId,
+                                                  @Param("date") LocalDate date,
+                                                  @Param("type") MealType type,
+                                                  Pageable pageable);
+
 
     Page<UserMeal> findByUserId(Long userId, Pageable pageable);
 }
